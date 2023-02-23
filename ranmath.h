f8ad0daab1d7999fc1f0808a07bfb2cd7fc6d5cc
Ransel117 <jonatanronnberg@gmail.com>
2023-02-23 19:24:49 +0100

    starting with neon

--- ranmath.h
+++ ranmath.h
@@ -398,10 +398,10 @@ RM_INLINE __m128 rmm_hadd4(__m128 a, __m128 b, __m128 c, __m128 d) {
 #if RM_NEON_ENABLE
 #include <arm_neon.h>
 
-#define rmm_load(v) v1d1q_f32(RM_VEC_CVT(v))
+#define rmm_load(v) vld1q_f32(RM_VEC_CVT(v))
 #define rmm_store(v, a) vst1q_f32(RM_VEC_CVT(v), a)
 #define rmm_set(x, y, z, w) rmm_load((vec4){x, y, z, w})
-#define rmm_set1(x) v1d1q_dup_f32(x)
+#define rmm_set1(x) vld1q_dup_f32(x)
 
 #endif /* RM_NEON_ENABLE */
 
